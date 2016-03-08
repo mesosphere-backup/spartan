257d552d359f21b5df97dc4e72daf65499617a10
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-03-08 10:28:08 -0800

    Extract exhibitor url.

--- include/spartan.hrl
+++ include/spartan.hrl
@@ -1,5 +1,3 @@
 -define(APP, spartan).
--define(EXHIBITOR_HOST, "http://master.mesos:8181").
--define(EXHIBITOR_URL, "/exhibitor/v1/cluster/status").
 -define(TLD, "zk").
 -define(ERLDNS_HANDLER, spartan_erldns_handler).
